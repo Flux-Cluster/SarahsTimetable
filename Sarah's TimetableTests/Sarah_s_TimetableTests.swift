348db21008788722fa09b388050f662ebe8ee39d
TelsApps <terry.s-h@hotmail.co.uk>
2024-12-01 17:46:18 +0000

    Initial Commit

---
+++ Sarah's TimetableTests/Sarah_s_TimetableTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sarah_s_TimetableTests.swift
+//  Sarah's TimetableTests
+//
+//  Created by Terry Spencer-Hopkins on 01/12/2024.
+//
+
+import Testing
+@testable import Sarah_s_Timetable
+
+struct Sarah_s_TimetableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
